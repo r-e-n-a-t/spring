154edd220a5c2143e2d26b5ab4d2921aef33fa95
r-e-n-a-t <ren2013ner@gmail.com>
2023-05-09 16:03:24 +1000

    change code

--- src/main/java/com/example/springsecurityapplication/controllers/MainController.java
+++ src/main/java/com/example/springsecurityapplication/controllers/MainController.java
@@ -96,38 +96,62 @@ public class MainController {
     public String productSearch(@RequestParam("search") String search, @RequestParam("ot") String ot, @RequestParam("do") String Do, @RequestParam(value = "price", required = false, defaultValue = "") String price, @RequestParam(value = "contract", required = false, defaultValue = "")String contract, Model model){
         model.addAttribute("products", productService.getAllProduct());
 
-        if(!ot.isEmpty() & !Do.isEmpty()){
-            if(!price.isEmpty()){
-                if(price.equals("sorted_by_ascending_price")) {
-                    if (!contract.isEmpty()) {
-                        if (contract.equals("furniture")) {
-                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
-                        } else if (contract.equals("appliances")) {
-                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
-                        } else if (contract.equals("clothes")) {
-                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
-                        }
-                    } else {
-                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
-                    }
-                } else if(price.equals("sorted_by_descending_price")){
-                    if(!contract.isEmpty()){
-                        System.out.println(contract);
-                        if(contract.equals("furniture")){
-                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 1));
-                        }else if (contract.equals("appliances")) {
-                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 3));
-                        } else if (contract.equals("clothes")) {
-                            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), 2));
-                        }
-                    }  else {
-                        model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
-                    }
-                }
-            } else {
-                model.addAttribute("search_product", productRepository.findByTitleAndPriceGreaterThanEqualAndPriceLessThanEqual(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+        if(!search.isEmpty() && (!ot.isEmpty() && !Do.isEmpty()) && !price.isEmpty() && !contract.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), getContractId(contract)));
+            } else{
+                model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), getContractId(contract)));
             }
-        } else {
+        }else if(!search.isEmpty() && (!ot.isEmpty() && !Do.isEmpty()) && !contract.isEmpty()){
+            model.addAttribute("search_product", productRepository.findByTitleAndCategoryOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do), getContractId(contract)));
+        }else if (!search.isEmpty() && (!ot.isEmpty() && !Do.isEmpty()) && !price.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByTitleOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+            }else{
+                model.addAttribute("search_product", productRepository.findByTitleOrderByPriceDesc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+            }
+
+        }else if (!search.isEmpty() && !contract.isEmpty() && !price.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByTitleAndCategoryAsc(search.toLowerCase(), getContractId(contract)));
+            } else{
+                model.addAttribute("search_product", productRepository.findByTitleAndCategoryDesc(search.toLowerCase(), getContractId(contract)));
+            }
+        }else if ((!ot.isEmpty() && !Do.isEmpty()) && !contract.isEmpty() && !price.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByCategoryOrderByPriceAsc(Float.parseFloat(ot), Float.parseFloat(Do), getContractId(contract)));
+            } else {
+                model.addAttribute("search_product", productRepository.findByCategoryOrderByPriceDesc(Float.parseFloat(ot), Float.parseFloat(Do), getContractId(contract)));
+            }
+        }else if (!search.isEmpty() && (!ot.isEmpty() && !Do.isEmpty())){
+            model.addAttribute("search_product", productRepository.findByTitleOrderByPriceAsc(search.toLowerCase(), Float.parseFloat(ot), Float.parseFloat(Do)));
+        }else if (!search.isEmpty() && !price.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByTitleAsc(search.toLowerCase()));
+            }else{
+                model.addAttribute("search_product", productRepository.findByTitleDesc(search.toLowerCase()));
+            }
+        }else if (!search.isEmpty() && !contract.isEmpty()){
+            model.addAttribute("search_product", productRepository.findByTitleAndCategory(search.toLowerCase(), getContractId(contract)));
+        }else if ((!ot.isEmpty() && !Do.isEmpty()) && !price.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByPriceAsc(Float.parseFloat(ot), Float.parseFloat(Do)));
+            }else{
+                model.addAttribute("search_product", productRepository.findByPriceDesc(Float.parseFloat(ot), Float.parseFloat(Do)));
+            }
+        }else if ((!ot.isEmpty() && !Do.isEmpty()) && !contract.isEmpty()){
+            model.addAttribute("search_product", productRepository.findByCategoryOrderByPrice(Float.parseFloat(ot), Float.parseFloat(Do), getContractId(contract)));
+        }else if (!price.isEmpty() && !contract.isEmpty()){
+            if(price.equals("sorted_by_ascending_price")) {
+                model.addAttribute("search_product", productRepository.findByCategoryAsc(getContractId(contract)));
+            } else {
+                model.addAttribute("search_product", productRepository.findByCategoryDesc(getContractId(contract)));
+            }
+        }else if (!ot.isEmpty() && !Do.isEmpty()){
+            model.addAttribute("search_product", productRepository.findByPrice(Float.parseFloat(ot), Float.parseFloat(Do)));
+        }else if (!contract.isEmpty()){
+            model.addAttribute("search_product", productRepository.findByCategory(getContractId(contract)));
+        }else{
             model.addAttribute("search_product", productRepository.findByTitleContainingIgnoreCase(search));
         }
 
@@ -138,6 +162,11 @@ public class MainController {
 
     }
 
+    public int getContractId(String contract){
+        if (contract.equals("beans")) {return 1;}
+        else if (contract.equals("ground")) {return 2;}
+        else {return 3;}
+    }
     @GetMapping("/cart/add/{id}")
     public String addProductInCart(@PathVariable("id") int id, Model model){
         // Получаем продукт по id
--- src/main/java/com/example/springsecurityapplication/repositories/ProductRepository.java
+++ src/main/java/com/example/springsecurityapplication/repositories/ProductRepository.java
@@ -13,25 +13,82 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     // Поиск всех продуктов по части наименования продукта в не зависомости от регистра
     List<Product> findByTitleContainingIgnoreCase(String name);
+    // Поиск всех продуктов фильтрация по диапазону цены
+    @Query(value = "select * from product where (price >= ?1 and price <= ?2)", nativeQuery = true)
+    List<Product> findByPrice(float ot, float Do);
+    // Поиск всех продуктов сортировка по возрастанию цены
+    @Query(value = "select * from product order by price",nativeQuery = true)
+    List<Product> findOrderAsc();
+    // Поиск всех продуктов сортировка по убыванию цены
+    @Query(value = "select * from product order by price desc",nativeQuery = true)
+    List<Product> findOrderDesk(String name);
+
+    // Поиск сортировка по возрастанию цены
+    @Query(value = "select * from product where (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price",nativeQuery = true)
+    List<Product> findByTitleAsc(String title);
+    // Поиск по категории
+    @Query(value = "select * from product where category_id = ?1",nativeQuery = true)
+    List<Product> findByCategory(int category);
+
+    // Поиск по наименованию, а также сортировка по убыванию цены
+    @Query(value = "select * from product where (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price desc",nativeQuery = true)
+    List<Product> findByTitleDesc(String title);
+    // Поиск по наименованию и фильтрация по категории
+    @Query(value = "select * from product where category_id = ?2 and(lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1')",nativeQuery = true)
+    List<Product> findByTitleAndCategory(String title, int category);
+    // Поиск по наименованию, сортировка по возрастанию цены, а также фильтрация по категории
+    @Query(value = "select * from product where category_id = ?2 and(lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price",nativeQuery = true)
+    List<Product> findByTitleAndCategoryAsc(String title, int category);
+
+    // Поиск по наименованию, сортировка по убыванию цены, а также фильтрация по категории
+    @Query(value = "select * from product where category_id = ?2 and(lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price desc",nativeQuery = true)
+    List<Product> findByTitleAndCategoryDesc(String title, int category);
+    // Поиск по диапазону цены, а также сортировка по возрастанию цены
+    @Query(value = "select * from product where (price >= ?1 and price <= ?2) order by price",nativeQuery = true)
+    List<Product> findByPriceAsc(float ot, float Do);
+
+    // Поиск по диапазону цены, а также сортировка по убыванию цены
+    @Query(value = "select * from product where (price >= ?1 and price <= ?2) order by price desc",nativeQuery = true)
+    List<Product> findByPriceDesc(float ot, float Do);
+    // Поиск по диапазону цены, сортировка по возрастанию цены,  а также фильтрация по категории
+    @Query(value = "select * from product where (price >= ?1 and price <= ?2) and category_id = ?3 order by price",nativeQuery = true)
+    List<Product> findByCategoryOrderByPriceAsc(float ot, float Do, int category);
+
+    // Поиск по диапазону цены, сортировка по убыванию цены,  а также фильтрация по категории
+    @Query(value = "select * from product where (price >= ?1 and price <= ?2) and category_id = ?3 order by price desc",nativeQuery = true)
+    List<Product> findByCategoryOrderByPriceDesc(float ot, float Do, int category);
+
+    // Поиск по диапазону цены, а также фильтрация по категории
+    @Query(value = "select * from product where (price >= ?1 and price <= ?2) and category_id = ?3",nativeQuery = true)
+    List<Product> findByCategoryOrderByPrice(float ot, float Do, int category);
+    // сортировка по возрастанию цены, а также фильтрация по категории
+    @Query(value = "select * from product where category_id = ?1 order by price",nativeQuery = true)
+    List<Product> findByCategoryAsc(int category);
+
+    // сортировка по убыванию цены, а также фильтрация по категории
+    @Query(value = "select * from product where category_id = ?1 order by price desc",nativeQuery = true)
+    List<Product> findByCategoryDesc(int category);
 
     // Поиск по наименованию и фильтрация по диапазону цены
     @Query(value = "select * from product where ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1')) and (price >= ?2 and price <= ?3)", nativeQuery = true)
     List<Product> findByTitleAndPriceGreaterThanEqualAndPriceLessThanEqual(String title, float ot, float Do);
 
     // Поиск по наименованию и фильтрация по диапазону цены, а также сортировка по возрастанию цены
-    @Query(value = "select * from product where (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') and (price >= ?2 and price <= ?3) order by price",nativeQuery = true)
+    @Query(value = "select * from product where (price >= ?2 and price <= ?3) and (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price",nativeQuery = true)
     List<Product> findByTitleOrderByPriceAsc(String title, float ot, float Do);
 
     // Поиск по наименованию и фильтрация по диапазону цены, а также сортировка по убыванию цены
-    @Query(value = "select * from product where (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') and (price >= ?2 and price <= ?3) order by price desc",nativeQuery = true)
+    @Query(value = "select * from product where (price >= ?2 and price <= ?3) and (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price desc",nativeQuery = true)
     List<Product> findByTitleOrderByPriceDesc(String title, float ot, float Do);
 
     // Поиск по наименованию и фильтрация по диапазону цены, сортировка по возрастанию цены,  а также фильтрация по категории
-    @Query(value = "select * from product where category_id = ?4 and(lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') and (price >= ?2 and price <= ?3) order by price",nativeQuery = true)
+    @Query(value = "select * from product where (price >= ?2 and price <= ?3) and category_id = ?4 and (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price",nativeQuery = true)
     List<Product> findByTitleAndCategoryOrderByPriceAsc(String title, float ot, float Do, int category);
 
     // Поиск по наименованию и фильтрация по диапазону цены, сортировка по убыванию цены,  а также фильтрация по категории
-    @Query(value = "select * from product where category_id = ?4 and(lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') and (price >= ?2 and price <= ?3) order by price desc",nativeQuery = true)
+    @Query(value = "select * from product where (price >= ?2 and price <= ?3) and category_id = ?4 and (lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') OR (lower(title) LIKE '%?1') order by price desc",nativeQuery = true)
     List<Product> findByTitleAndCategoryOrderByPriceDesc(String title, float ot, float Do, int category);
+
+
 }
 
